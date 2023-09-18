57d9145c234d7d2892949c82a4a50a45c188bb7f
E127214 <kemal.taufik@aia.com>
2023-09-18 15:11:44 +0800

    compare image speed

--- pages/first-post.js
+++ pages/first-post.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from 'next/image';
+
 
 const FirstPost = () => {
   return (
-    <div>First Post</div>
+    <>
+        <img src="/images/profile.jpg" alt="Your Name" />
+        <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+        <div>First Post</div>
+    </>
+    
   )
 }
 
--- pages/second-post.js
+++ pages/second-post.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Image from 'next/image';
 const secondPost = () => {
   return (
+    <>
     <div>secondPost</div>
-  )
+    <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+    </>
+    )
 }
 
 export default secondPost
