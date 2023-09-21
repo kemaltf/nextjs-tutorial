667153a253f713e8fdc0290cc300cb638992c2f9
E127214 <kemal.taufik@aia.com>
2023-09-21 17:12:17 +0800

    best practice getstaticpaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -14,18 +14,17 @@ const Post = ({post}) => {
 export default Post
 
 export async function getStaticPaths(){
+    const response = await fetch('http://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    const paths = data.map(post=>{
+        return {
+            params: {
+                postId: `${post.id}`
+            }
+        }
+    })
     return {
-        paths: [
-            {
-                params:{postId:'1'}
-            },
-            {
-                params:{postId:'2'}
-            },
-            {
-                params:{postId:'3'}
-            },
-        ],
+        paths,
         fallback: false
     }
 }
